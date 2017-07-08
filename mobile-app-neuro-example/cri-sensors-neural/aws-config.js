8c23b943c6a8db5364f99090b289f14f209d21f5
Curt White <curt3n5@gmail.com>
2017-07-08 10:29:37 +0200

    config

--- mobile-app-neuro-example/cri-sensors-neural/aws-config.js
+++
@@ -1,9 +0,0 @@
-evothings.aws = evothings.aws || {}
-
-// TODO: Fill in your AWS information here.
-evothings.aws.config = {
-	accessKeyId: 'AKIAIFR4XN6UZGI46QLA',
-	secretAccessKey: '1xLW0djL2kI0tbe6abnS3eHDmHcNHX7GwASmApE3',
-	region: 'us-west-1',
-	params: { FunctionName: 'IOE2015-IoTAPI-DWRZ6BEMMMEI' }
-}
